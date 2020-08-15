ab1567db93c76579d3dd9b411660b489898e7007
susanforme <rzc307853639@gmail.com>
2020-08-15 21:53:58 +0800

    大数

---
+++ src/java的基本设计结构/exercise/BigNumLotteryOdds.java
@@ -0,0 +1,26 @@
+package java的基本设计结构.exercise;
+/*
+ * @Author susan .
+ * @Email: rzc307853639@gmail.com.
+ * @Date time: 2020/8/15 21:03.
+ * @Description 兼容大数的抽奖器.
+ */
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class BigNumLotteryOdds {
+  public static void main(final String[] args) {
+    System.out.println("你需要最大的数");
+    final Scanner scan = new Scanner(System.in);
+    final int max = scan.nextInt();
+    System.out.println("你希望抽出几个数");
+    final int num = scan.nextInt();
+    BigInteger odd = BigInteger.valueOf(1);
+    scan.close();
+    for (int i = 1; i <= num; i++) {
+      odd = odd.multiply(BigInteger.valueOf(max - i + 1)).divide(BigInteger.valueOf(i));
+    }
+    System.out.println(odd);
+  }
+}
--- src/java的基本设计结构/note/BigNumber.java
+++ src/java的基本设计结构/note/BigNumber.java
@@ -6,6 +6,18 @@ package java的基本设计结构.note;
  * @Description 大数.
  */
 
+import java.math.BigInteger;
+
 public class BigNumber {
-  public static void main(final String[] args) {}
+  /* 如果基本的整数和浮点数不能满足需求,那么就可以使用java.math包中的类,BigInteger
+   * BigDecimal,BigInteger任意长度的整数运算,BigDecimal 任意长度的浮点数运算
+   */
+  public static void main(final String[] args) {
+    final BigInteger a = BigInteger.valueOf(100);
+    final BigInteger b = BigInteger.valueOf(100);
+    // 大数不能使用+ * 运算
+    final BigInteger c = a.add(b);
+    final BigInteger d = a.multiply(b);
+    System.out.println("this is c: " + c + ",this is d: " + d);
+  }
 }
