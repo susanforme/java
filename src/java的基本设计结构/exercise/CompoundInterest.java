7279307a7007aca41bff0ff5efde1a026edcd03d
susanforme <rzc307853639@gmail.com>
2020-08-24 21:51:29 +0800

    array

---
+++ src/java的基本设计结构/exercise/CompoundInterest.java
@@ -0,0 +1,31 @@
+package java的基本设计结构.exercise;
+/*
+ * @Author susan .
+ * @Email: rzc307853639@gmail.com .
+ * @Date time: 2020/8/24 21:26 .
+ * @Description 利率生成计算
+ */
+
+public class CompoundInterest {
+  public static void main(final String[] args) {
+    final int YEARS = 10;
+    final int INTEREST = 6;
+    final double[][] balances = new double[INTEREST][YEARS];
+    for (int k = 0; k < INTEREST; k++) {
+      balances[k][0] = 10000;
+    }
+    for (int i = 0; i < balances.length; i++) {
+      for (int j = 1; j < balances[i].length; j++) {
+        final double lastYearMoney = balances[i][j - 1];
+        balances[i][j] = lastYearMoney * (1.1 + 0.01 * i);
+      }
+    }
+    for (final double[] k : balances) {
+      for (final double j : k) {
+        System.out.printf("%10.2f", j);
+      }
+      System.out.println("\n");
+    }
+    //    System.out.println(Arrays.deepToString(balances));
+  }
+}
--- src/java的基本设计结构/note/MyArray.java
+++ src/java的基本设计结构/note/MyArray.java
@@ -1,17 +1,29 @@
 package java的基本设计结构.note;
 /*
  * @Author susan .
- * @Email: rzc307853639@gmail.com.
- * @Date time: 2020/8/19 20:34.
- * @Description java数组
+ * @Email: rzc307853639@gmail.com .
+ * @Date time: 2020/8/19 20:34 .
+ * @Description java数组 .
  */
 
 import java.util.Arrays;
 
 public class MyArray {
   public static void main(final String[] args) {
-    // 数组排序
-    final int[] a = new int[] {1, 2};
-    System.out.print(Arrays.binarySearch(a, 0, a.length, 2));
+    // 多维数组
+    final double[][] balances = new double[3][3];
+    final double[][] magics = new double[][] {{1, 2, 3}, {1, 2, 3}};
+    for (int i = 0; i < balances.length; i++) {
+      for (int j = 0; j < balances[i].length; j++) {
+        balances[i][j] = Math.random();
+      }
+    }
+    System.out.println(Arrays.deepToString(balances));
+    //    System.out.println(Arrays.deepToString(magics));
+    for (final double[] i : magics) {
+      for (final double j : i) {
+        System.out.print(j + ", ");
+      }
+    }
   }
 }
