a6f6ac61138d15e3d77f814b853ab23de64f7363
susanforme <rzc307853639@gmail.com>
2020-09-01 21:24:03 +0800

    对象

--- src/objectAndClass/exercise/EmployeeTest.java
+++ src/objectAndClass/exercise/EmployeeTest.java
@@ -12,10 +12,18 @@ public class EmployeeTest {
   public static void main(final String[] args) {
     final Employee[] employees = new Employee[3];
     for (int i = 0; i < employees.length; i++) {
-      employees[i] = new Employee("诚诚", (int) Math.round(Math.random() * 20), 3000);
+      employees[i] =
+          new Employee(
+              "诚诚", (int) Math.round(Math.random() * 20), Math.round(Math.random() * 3000));
     }
     for (final Employee e : employees) {
+      System.out.println(e.getAge());
       System.out.println(e.logAllMsg());
     }
+    for (final Employee e : employees) {
+      System.out.println(e.riseSalary());
+      System.out.println("id是: " + e.getId() + ",静态属性下一个id是: " + e.getNextId());
+    }
+    System.out.print(employees[0].equals(employees[1]));
   }
 }
--- src/objectAndClass/note/Employee.java
+++ src/objectAndClass/note/Employee.java
@@ -6,22 +6,49 @@ package objectAndClass.note;
  * @Description .
  */
 
+import java.util.Objects;
+
 public class Employee {
   private final String name;
   private final int age;
-  private final double salary;
+  private double salary;
+  /** 下一个id */
+  private static int nextId = 0;
+  /** 每个员工的id */
+  private int id = 0;
+  /** 静态常量,公司成立时间 */
+  private static final int year = 2000;
 
   public Employee(final String n, final int a, final double s) {
-    name = n;
-    age = a;
-    salary = s;
+    this.name = n;
+    this.age = a;
+    this.salary = s;
+    this.id = nextId;
+    nextId++;
   }
   /** 输出所有信息 */
   public String logAllMsg() {
-    return "员工" + name + "年龄" + age + "的薪水是" + salary;
+    return "员工" + this.name + "年龄" + this.age + "的薪水是" + this.salary;
   }
   /** 年龄 */
   public int getAge() {
     return age;
+  }
+  /** 涨薪水,同时返回薪水 */
+  public double riseSalary() {
+    this.salary *= 1.05;
+    return this.salary; // 可以不使用this,但是建议使用
+  }
+  /** 比较是否相等 */
+  public boolean equals(final Employee other) {
+    return Objects.equals(this.name, other.name);
+  }
+  /** 取得id */
+  public int getId() {
+    return this.id;
+  }
+  /** 下一个id是 */
+  public int getNextId() {
+    return nextId;
   }
 }
