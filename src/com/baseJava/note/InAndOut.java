ef911427ecf09a9a5b7aa96fc0d64219524ca9a7
susanforme <rzc307853639@gmail.com>
2020-09-15 21:54:20 +0800

    注释

--- src/com/baseJava/note/InAndOut.java
+++ src/com/baseJava/note/InAndOut.java
@@ -2,14 +2,13 @@ package com.baseJava.note;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.nio.charset.StandardCharsets;
 
 public class InAndOut {
   public static void main(final String[] args) {
     // 文件的写入
     try {
       // 相对路径报错文件不存在,文件名则会在根目录创建
-      final PrintWriter out = new PrintWriter("com/baseJava/hello.txt", StandardCharsets.UTF_8);
+      PrintWriter out = new PrintWriter("com/baseJava/hello.txt");
       // 像使用System.out一样使用
       out.println("wo ai ni");
       out.close();
---
+++ src/doc/Comment.java
@@ -0,0 +1,18 @@
+package doc;
+
+/*
+ * @Author 不灵不落我是个机器人 .
+ * @Email: rzc307853639@gmail.com .
+ * @Date time: 2020/09/15/21:20 .
+ * @Description .
+ */
+
+/** 类注释测试 */
+public class Comment {
+  private String myComment;
+
+  /** 生成器 */
+  public Comment() {
+    this.myComment = "HelloWorld.java";
+  }
+}
